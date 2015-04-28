7d5214d01e8844a314d4a5aea6a4e35afa19f729
Tomoka Baba <baba@robox.org>
2015-04-28 12:30:34 +0900

    [add] list

---
+++ list.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+sales= [255, 100, 353, 400]
+print len(sales)
+print sales[2]
+sales[2] = 100
+print sales[2]
+
+# 含んでいるか否か
+print 100 in sales
+print 500 in sales
+
+# range
+print range(10)
+print range(3,10)
+print range(3,10,2)
